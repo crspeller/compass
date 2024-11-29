164379761a2bb1e31290f19d8c90b00e0d1aa4d7
Christopher Speller <crspeller@gmail.com>
2024-11-29 12:00:28 +0100

    feat: Add ButtonSize and ButtonVariant imports to TestApp

--- test-app/TestApp.tsx
+++ test-app/TestApp.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Button, Checkbox } from '../src/index';
+import { Button, ButtonSize, ButtonVariant, Checkbox, CheckboxSize } from '../src/index';
 import { createGlobalStyle } from 'styled-components';
 import { defaultTheme } from './theme';
 
